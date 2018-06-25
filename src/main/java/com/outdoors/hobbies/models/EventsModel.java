12c28505f6a0b84d208439954277b904a2e37a0a
LHristozov <luboih@gmail.com>
2018-06-25 23:35:50 +0300

    minor changes

--- src/main/java/com/outdoors/hobbies/models/EventsModel.java
+++ src/main/java/com/outdoors/hobbies/models/EventsModel.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "EVENTS")
@@ -29,6 +31,8 @@ public class EventsModel {
 	private String name;
 	
 	@NotNull
+	@Size(min = 1, max = 4000)
+	@Column(name = "description")
 	private String description;
 	
 	@NotNull
